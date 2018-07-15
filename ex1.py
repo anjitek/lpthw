8868a10f22c591e8872af157625fe368bdea7219
anjitek <57280880@qq.com>
2018-07-15 15:35:15 +0800

    # try commit

---
+++ ex1.py
@@ -0,0 +1,9 @@
+print('Hello world')
+print('Hello, again')
+print('this is running ~~~~~~~')
+print('this is a \'trying\'.')
+print('Octothorpe test below')
+#111111111111
+#222222222222
+#asdfasdlkfjlasdf
+print('Octothorpe test done!')
---
+++ ex11.py
@@ -0,0 +1,9 @@
+print("How old are you?", end = ' ')
+age = input()
+print("How tall are you?", end = ' ')
+height = input()
+print("howmuch do you weigh?", end = ' ')
+# a space at the end of line “ ”, makes the input don't change line
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
