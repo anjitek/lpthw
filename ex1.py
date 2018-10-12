2f12d52f9358e0a3966a534aa595a789c862c993
Albert <57280880@qq.com>
2018-10-12 13:56:14 +0800

    20181012.v2

--- ex1.py
+++ ex1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 print('Hello world')
 print('Hello, again')
 print('this is running ~~~~~~~')
@@ -8,14 +7,3 @@ print('Octothorpe test below')
 #222222222222
 #asdfasdlkfjlasdf
 print('Octothorpe test done!')
-=======
-print('Hello world')
-print('Hello, again')
-print('this is running ~~~~~~~')
-print('this is a \'trying\'.')
-print('Octothorpe test below')
-#111111111111
-#222222222222
-#asdfasdlkfjlasdf
-print('Octothorpe test done!')
->>>>>>> d24a6d876ed3850fc689e444bd678b67c7fe3e4d
--- ex39.py
+++ ex39.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # this is ex39
 # create a mapping of state to abreviation
 
@@ -9,15 +8,3 @@ states = {
     'New York' : 'NY'
     'Michigan' : 'MI'
 }
-=======
-# this is ex39
-# create a mapping of state to abreviation
-
-states = {
-    'Oregon' : 'OR',
-    'Florida' : 'FL'
-    'California' : 'CA'
-    'New York' : 'NY'
-    'Michigan' : 'MI'
-}
->>>>>>> d24a6d876ed3850fc689e444bd678b67c7fe3e4d
