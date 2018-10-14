11ae7992061e4d764ae70001c113b6783d3701ee
Albert <57280880@qq.com>
2018-10-14 17:06:20 +0800

    Ex41,py, no idea what it is, v3.

--- ex41.py
+++ ex41.py
@@ -3,7 +3,7 @@ import random
 from urllib.request import urlopen
 import sys
 
-WORD_URL = "http://learncodethehardway.org/word.txt"
+WORD_URL = "http://learncodethehardway.org/words.txt"
 WORDS = []
 
 PHRASES = {
