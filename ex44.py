7c1ecbe70e0dae92d3a3c3792b67c45d52a99b0a
Albert <57280880@qq.com>
2018-10-28 00:55:05 +0800

    this is ex44, three combined.

--- ex44.py
+++ ex44.py
@@ -5,11 +5,31 @@ class Parent(object):
     def implicit(self):
         print("PARENT implicit()")
 
+    def override(self):
+        print("Parent override()")
+
+    def altered(self):
+        print("Parent altered()")
+
 class Child(Parent):
-    pass
+
+    def override(self):
+        print("Child override()")
+
+    def altered(self):
+        print("Child, beeeeeeeeefore Parent altered()")
+        super(Child, self).altered() # this line is important, inherit from parent class
+        print("CHILD, A---FTER Parent altered()")
+
 
 dad = Parent()
 son = Child()
 
 dad.implicit()
 son.implicit()
+
+dad.override()
+son.override()
+
+dad.altered()
+son.altered()
